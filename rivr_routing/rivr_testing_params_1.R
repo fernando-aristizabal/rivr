feda574b67978c75f915889428b664731b069403
fernando-aristizabal <fernandoa@disroot.org>
2021-06-22 13:38:52 +0000

    Updates * removed postgres * removed entrypoint file * adding umask in test script * changed parameter values

--- rivr_routing/rivr_testing_params_1.R
+++ rivr_routing/rivr_testing_params_1.R
@@ -3,13 +3,13 @@
 ################################################################################
 
 ## channel slope ( vertical m / horizontal m) ##
-slopes <- seq(0.001,0.01,0.002)
+slopes <- seq(0.001,0.009,0.002)
 ## channel length (m) ##
 extents <- 1500
 ## Manning's roughness ##
-mannings <- seq(0.10,0.20,0.02)
+mannings <- seq(0.10,0.20,0.25)
 ## channel bottom width (m) ## 
-widths <- seq(20,50,5)
+widths <- seq(20,50,10)
 ## channel side slope (horizontal m / vertical m) ##
 sideslopes <- seq(0,1,0.25)
 ## time step (s) ##
@@ -26,6 +26,6 @@ g <- 9.81
 # should be less than 0.06 for Dynamic wave
 desired_courant_number <- 0.05
 ## output file template name ##
-output_file_name <- '/data/outputs_20210611/rivr_output_params.h5'
+output_file_name <- '/data/outputs_20210614/rivr_output_params.h5'
 ## cores to use ##
 cores <- 7
